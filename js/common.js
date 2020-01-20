ab9c1c2720b124c327bf07dfec976cd3d0924680
hyejinni <kimhj8463@gmail.com>
2020-01-20 18:28:00 +0900

    인스타 계정 관련
    User ID, Access Token 추가

--- js/common.js
+++ js/common.js
@@ -72,8 +72,8 @@ $(document).ready(function() {
     get: 'user',
     limit: 6,
     resolution: 'standard_resolution',
-    userId: '8987997106',
-    accessToken: '8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9',
+    userId: '3595725576',
+    accessToken: '3595725576.1677ed0.22c23d71cf3d4008ae214c25bd251476',
     template:
       '<li class="instagram-item"><a href="{{link}}" aria-label="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></li>'
   });
